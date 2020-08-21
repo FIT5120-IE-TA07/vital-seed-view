47706bc9b287fac01f81a3e484dc5a3a5014bf90
Sid Raj <siddharth.rajesh13@gmail.com>
2020-08-21 22:01:36 +1000

    created button to test API call

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function testApi() {
+    fetch("http://localhost:9000/api")
+      .then(res => {
+        console.log(res)
+        alert(res)
+      })
+      .catch(err => {
+        alert(err)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={testApi}>Click Me!</button>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <TestApi/>
+    // </div>
   );
 }
 
